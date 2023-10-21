30308fcad9c3bbab704eb6d5f629a9e0891865b9
Andrew Kennedy <92186003+andyk8872@users.noreply.github.com>
2023-10-21 12:03:48 +0100

    add/edit testimonials page

--- assets/js/testimonial.js
+++ assets/js/testimonial.js
@@ -1,54 +1,65 @@
 // Initialize reviews array with data from local storage or an empty array if no data is available
 const reviews = JSON.parse(localStorage.getItem('reviews')) || [];
+
 // Function to save reviews to local storage
 function saveReviews() {
     localStorage.setItem('reviews', JSON.stringify(reviews));
-}    
+} 
 
-let enterButton = document.getElementsByClassName('submit');
-enterButton[0].addEventListener('click', submitReview);
-
-function submitReview() {
-    // Get user inputs
-    const name = document.getElementById('name').value;
-    const reviewText = document.getElementById('review').value;
-
-    if (name && reviewText) {
-        // Create a new review object
-        const review = { name, reviewText };
-
-        // Add the review to the array
-        reviews.push(review);
-
-        // new
-          // Save the reviews to local storage
-          saveReviews();
-
-        // Clear the input fields
-        document.getElementById('name').value = '';
-        document.getElementById('review').value = '';
-
-        // Display the reviews
-        displayReviews();
-    } else {
-        alert('Please fill out both fields before submitting a review.');
-    }
+// Function to add a review
+function addReview(name, review) {
+    const reviewItem = {
+        name: name,
+        review: review
+    };
+    reviews.push(reviewItem);
+    displayReviews();
 }
+
 // new
 // Load and display reviews when the page loads
 window.addEventListener('load', () => {
     displayReviews();
 });
+// un-comment to clear local storage
+// localStorage.clear();
 
+// Function to display reviews
 function displayReviews() {
-    const reviewsContainer = document.querySelector('.reviews');
-    reviewsContainer.innerHTML = '';
+    const reviewList = document.getElementById("reviewList");
+    reviewList.innerHTML = "";
 
-    // Loop through the reviews and display them
     reviews.forEach((review, index) => {
-        const reviewElement = document.createElement('div');
-        reviewElement.classList.add('review');
-        reviewElement.innerHTML = `<strong>${review.name}:</strong><br>${review.reviewText}`;
-        reviewsContainer.appendChild(reviewElement);
+        const randomImageNumber = Math.floor(Math.random() * 3) + 1;
+        const card = document.createElement("div");
+        card.classList.add("col-md-6", "mb-3");
+
+        card.innerHTML = `
+            <div class="card border border-4 border-warning">                
+                <img src="/assets/images/${randomImageNumber}.jpg" class="card-img-top" alt="User Image">
+                <div class="card-body">
+                    <h5 class="card-title">${review.name}</h5>
+                    <p class="card-text">${review.review}</p>
+                </div>
+            </div>
+        `;
+
+        reviewList.appendChild(card);
     });
 }
+
+// Submit review form
+const reviewForm = document.getElementById("reviewForm");
+reviewForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const review = document.getElementById("review").value;
+
+    addReview(name, review);
+     // Save the reviews to local storage
+     saveReviews();
+
+    // Reset the form
+    reviewForm.reset();
+});
